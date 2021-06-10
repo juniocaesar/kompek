52d32244aadb4c62e585b95dd3bafab5235e6c36
Junio Caesar <junio.caesar@gmail.com>
2021-06-11 02:31:15 +0700

    Registration_Form_Dev_Deploy

---
+++ pages/announcement.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Navbar from "./components/navbar";
+// import { Input, Box, Text, Button } from "@chakra-ui/react";
+// import { db } from "../config/firebaseConfig";
+// import uploadImage from "../bin/uploadImage";
+
+function Announcement() {
+  return (
+    <div className="announcement">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default Announcement;
---
+++ pages/submission.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Navbar from "./components/navbar";
+// import { Input, Box, Text, Button } from "@chakra-ui/react";
+// import { db } from "../config/firebaseConfig";
+// import uploadImage from "../bin/uploadImage";
+
+function Submission() {
+  return (
+    <div className="submission">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default Submission;
